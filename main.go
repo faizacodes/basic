bfef1fb61bcdb136540e9af44a1e297f0116c9fb
faizacodes <faizatemirova5e4@gmail.com>
2022-11-11 23:29:08 +0600

    average numbers function

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt" 
+)
+
+func sum(array []int) int {  
+	result := 0  
+	for _, v := range array {  
+	 result += v  
+	}  
+	return result  
+   }  
+
+func averageNumbers(numbers[]int) float32{
+	return float32(sum(numbers)) / float32(len(numbers))
+}
+
+func main() {
+	fmt.Printf("Avarage numbers [10,4,6,5,7,5]: %.1f", averageNumbers([]int{10,4,6,5,7,5,5,6,5}))
+
+}
+
+
