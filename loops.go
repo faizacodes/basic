5c0eee99a5202d6bd0516d27fcc7e3826cfed5e8
temirovazat <azatbekt5@gmail.com>
2022-11-12 15:23:40 +0600

    add loops and new functions

---
+++ loops.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func readLine(greeting string) string{
+	fmt.Print(greeting)
+	reader := bufio.NewReader(os.Stdin)
+	text, _, _:= reader.ReadLine()
+	return string(text)
+}
+
+
+func main() {
+	for i:=1; i < 10; i+=2{
+		fmt.Println(i)
+	}
+	var country = readLine("Where are you from: ")
+	if country == "Kyrgyzstan" || country == "Kazakhstan" || country == "Uzbekistan" || country == "Tajikistan" || country == "Turkmenistan"{
+		fmt.Println("You from Central Asia")
+	}else{
+		fmt.Println("You from other country")
+	}
+}
